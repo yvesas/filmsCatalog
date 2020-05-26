f51b6d09e8a29dcb8b256ee9efbd6c7e1ca31b93
Yves Siqueira <yves.as@gmail.com>
2020-05-26 19:40:03 -0300

    teste com flip

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -8,7 +8,9 @@ export default class Main extends Component {
     items: [],
     resultInfo: {},
     page: 1,
-    query: ''
+    query: '',
+    flipping: false,
+    flippingKey: 0
   }
 
   apiKey = "e83f89ff8e1495b5203d7218d3095d9b"
@@ -53,9 +55,16 @@ export default class Main extends Component {
   
   keyPress = (e) => {
     if(e.keyCode == 13){
-      this.setState({ query: e.target.value });
+      this.setState({ query: e.target.value })
       this.loadItems(this.state.query)
     }
+  }
+
+  flippingThrough = (key) => {
+    this.setState({ flipping: !this.state.flipping, flippingKey: key})
+
+    console.log(key)
+    console.log(this.state)
   }
 
   render(){
@@ -67,13 +76,22 @@ export default class Main extends Component {
       
         <div > 
           {items.map(item => (
-            <div className="item-card row" key={item.id}>
-              <div className="column1">
-                <img src={`https://image.tmdb.org/t/p/w94_and_h141_bestv2${item.backdrop_path}`}></img>
+            <div className="item-card" key={item.id}>
+              <div className="poster">
+                <img src={`https://image.tmdb.org/t/p/w94_and_h141_bestv2${item.backdrop_path}`} onClick={this.flippingThrough}></img>
               </div>  
-              <div className="column2">
-              <strong>{item.title || item.name}</strong>
-              <p>{item.overview}</p>
+              <div className="infos flip-box">              
+                <div className={this.state.flipping ? 'flip-box-inner flipping' : 'flip-box-inner'}>
+                  <div className="flip-box-front"> 
+                    <strong>{item.title || item.name}</strong>
+                    <p>{item.overview}</p>
+                  </div>
+                  <div className="flip-box-back">
+                      <strong>{item.title || item.name}</strong>
+                      <p>Data de lançamento: {item.release_date || item.first_air_date}</p>
+                      <p>Avaliação: {item.vote_average}</p>
+                  </div>
+                </div>
               </div>
             </div>
           ))}
