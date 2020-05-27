b3969c8189509aa3b89cfef1e83b6972cac0aa05
Yves Siqueira <yves.as@gmail.com>
2020-05-27 19:03:56 -0300

    retirado mui-treasure lib

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,13 +1,20 @@
 import React, {Component}  from 'react';
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
+
+import cx from 'clsx';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import Divider from '@material-ui/core/Divider';
+import Rating from '@material-ui/lab/Rating';
+import ArrowForwardIos from '@material-ui/icons/ArrowForwardIos';
+import ModeComment from '@material-ui/icons/ModeComment';
+import Favorite from '@material-ui/icons/Favorite';
 
 import { makeStyles } from '@material-ui/core/styles';
 import api from '../../services/api';
@@ -71,7 +78,6 @@ export default class Main extends Component {
 
   render(){
     const {items, page, resultInfo} = this.state;
-
     const classes = makeStyles((theme) => ({
       intro: {
         backgroundColor: theme.palette.background.paper,
@@ -97,7 +103,50 @@ export default class Main extends Component {
       },
     }));
 
-
+    const styles =  makeStyles(({ spacing, palette }) => ({
+      card: {
+        display: 'flex',
+        padding: spacing(2),
+        borderRadius: 16,
+      },
+      media: {
+        minWidth: '25%',
+        maxWidth: '25%',
+        flexShrink: 0,
+        backgroundColor: palette.grey[200],
+        borderRadius: 12,
+        boxShadow: '0 2px 8px 0 #c1c9d7, 0 -2px 8px 0 #cce1e9',
+      },
+      rating: {
+        verticalAlign: 'text-top',
+      },
+      content: {
+        padding: spacing(0, 2, 0, 0),
+      },
+      heading: {
+        fontSize: 17,
+        fontWeight: 'bold',
+        letterSpacing: '0.5px',
+        marginBottom: 0,
+        marginRight: spacing(1.5),
+        display: 'inline-block',
+      },
+      body: {
+        fontSize: 14,
+        color: palette.grey[500],
+      },
+      divider: {
+        margin: spacing(1, 0),
+      },
+      textFooter: {
+        fontSize: 14,
+      },
+      icon: {
+        fontSize: '1.2rem',
+        verticalAlign: 'bottom',
+      },
+    }));
+    
     return (
       <div>
         <div className={classes.intro}>
@@ -116,7 +165,7 @@ export default class Main extends Component {
           <Grid container spacing={4}>
             {items.map((item) => (
               <Grid item key={item.id} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
+                {/* <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
                     component="img"
@@ -132,7 +181,34 @@ export default class Main extends Component {
                       {item.overview}
                     </Typography>
                   </CardContent>
-                </Card>
+                </Card> */}
+              
+              <Card className={styles.card}>
+                <CardMedia
+                  className={styles.media}
+                  component="img"
+                  image={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${item.backdrop_path}`}
+                  title={item.title || item.name}
+                  alt={item.title || item.name}
+                />
+                <CardContent className={styles.content}>
+                  <Box mb={1}>
+                    <h3 className={styles.heading}>{item.title || item.name}</h3>
+                    <Rating
+                      name={'rating'}
+                      value={2}
+                      className={styles.rating}
+                      size={'small'}
+                    />
+                  </Box>
+                  <p className={styles.body}>
+                    {item.overview}
+                  </p>
+                </CardContent>
+                
+              </Card>
+
+
               </Grid>
             ))}
           </Grid>
