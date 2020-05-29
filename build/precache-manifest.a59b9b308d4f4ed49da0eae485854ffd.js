9baf5d17a827dea2e88357c9a421bb91cb579138
Yves Siqueira <yves.as@gmail.com>
2020-05-29 15:07:34 -0300

    testando Netlify

---
+++ build/precache-manifest.a59b9b308d4f4ed49da0eae485854ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4558b4fc402d975dc2c34ddc1be9f8e",
+    "url": "/filmsCatalog/index.html"
+  },
+  {
+    "revision": "bf1359c962ecc48af189",
+    "url": "/filmsCatalog/static/js/2.cc54ff84.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/filmsCatalog/static/js/2.cc54ff84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63678313dd23d9db8fa2",
+    "url": "/filmsCatalog/static/js/main.cd71142a.chunk.js"
+  },
+  {
+    "revision": "95a163b65ecb0d5d37b4",
+    "url": "/filmsCatalog/static/js/runtime-main.d93366a8.js"
+  },
+  {
+    "revision": "56fd61eec148690af48f876dfd5c8718",
+    "url": "/filmsCatalog/static/media/logo.56fd61ee.svg"
+  }
+]);
